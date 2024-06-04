190da9ebe2728563f982d5ffa1c900d56f0db27b
Rafademetrio <rafademetrio@gmail.com>
2024-06-03 21:04:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'sos.morro',
+  appName: 'sos-morro',
+  webDir: 'www'
+};
+
+export default config;
